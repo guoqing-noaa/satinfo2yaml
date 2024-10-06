5483364c1d8a41fd051ac9340f6bc34c7300f93b
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-10-06 12:19:51 -0600

    update yamltool.py

--- yamltool.py
+++ yamltool.py
@@ -6,7 +6,7 @@ def traverse(subdata,n): # traverse the yaml dict tree until reaching leafs
   if isinstance(subdata,dict):
     n=n+1
     for key,value in subdata.items():
-      print(f"{' '*n*3}{key}")
+      print(f"{' '*(n-1)*3}{key}")
       traverse(value,n)
 
 def getFinalValue(subdata,keytree): # get the value for a hirearchy key string
@@ -21,31 +21,31 @@ def getFinalValue(subdata,keytree): # get the value for a hirearchy key string
 args=sys.argv
 nargs=len(args)-1
 if nargs <1:
-  print(f"Usage: {args[0]} <file> [keystr] [traverse|dump|changeto='']")
+  print(f"Usage: {args[0]} <file> [keystr] [traverse|dump|changeto=''] #default action is traverse")
   exit()
 myfile=args[1]
 mykeystr=""
 if nargs >1:
   mykeystr=args[2]
+action="traverse"
+if nargs>2:
+  action=args[3]
 
 with open(myfile) as yfile:
   data=yaml.safe_load(yfile)
 
-# list the top-level keys
-for key in data.keys():
-  print(f"{key}")
 
 if mykeystr:
-  print(f"\n=== {mykeystr} ==")
+  print(f"\n## === {mykeystr} ==")
   keytree=mykeystr.split("/")
-  print(getFinalValue(data,keytree))
-  exit()
-  
-  subdata=data.get(mykeystr)
-  traverse(subdata,1)
-  exit()
-  if isinstance(subdata,dict):
-    for key in subdata.keys():
-      print(key)
-    print(f"==== subdata ======")
+  subdata=(getFinalValue(data,keytree))
+
+  if action=="traverse":
+    traverse(subdata,0)
+  elif action=="dump":
     yaml.dump(subdata, sys.stdout)
+
+else:
+  # list the top-level keys
+  for key in data.keys():
+    print(f"{key}")
